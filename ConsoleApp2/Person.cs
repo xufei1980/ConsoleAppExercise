34bc0eac2bb84162538334a6f51f6984b2a01adc
xufei1980 <xf2100@gmail.com>
2019-09-23 14:04:35 +1200

    添加项目文件。

---
+++ ConsoleApp2/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Person
+    {
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+
+        public int Age { set; get; }
+
+        public void SayHello()
+        {
+            Console.WriteLine($"hello I am {FirstName} {LastName}");
+        }
+        public void SetAge(int n)
+        {
+            Age = n;
+        }
+    }
+    public class Student : Person
+    {
+        public void GoToClass()
+        {
+            Console.WriteLine("I am going to class");
+        }
+
+        public void ShowAge()
+        {
+            Console.WriteLine($"I am {Age} year old");
+        }
+    }
+
+    public class Teacher : Person
+    {
+        public void Explain()
+        {
+            Console.WriteLine("Explaination begins");
+        }
+    }
+}
---
+++ ConsoleApp2/StudentAndTeacherTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class StudentAndTeacherTest
+    {
+        static void Main(string[] args)
+        {
+            var aPerson = new Person
+            {
+                FirstName = "Alix",
+                LastName = "Hill"
+              };
+            aPerson.SayHello();
+            var aStudent = new Student
+            {
+                FirstName = "Bill",
+                LastName = "Wang"
+            };
+            aStudent.SetAge(21);
+            aStudent.SayHello();
+            aStudent.ShowAge();
+            var aTeacher = new Teacher
+            {
+                FirstName = "Mia",
+                LastName = "Lynn"
+            };
+            aTeacher.SetAge(31);
+            aTeacher.SayHello();
+           
+            Console.ReadLine();
+        }
+    }
+}
